9f1e7beb3fa0721c24cf81151ba86eeb3a002aae
Rishabh Pathak <clusmsyknight@gmail.com>
2022-07-20 17:08:45 +0530

    added TextOulined widget, just in case need somewhere

---
+++ lib/common/widgets/TextOutlined.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class TextOutlined extends StatelessWidget {
+  final String text;
+  final Color color;
+  final Color borderColor;
+  final double fontSize;
+
+  const TextOutlined({
+    Key? key,
+    required this.text,
+    this.color = Colors.white,
+    this.borderColor = Colors.black,
+    this.fontSize = 20,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // https://stackoverflow.com/a/53211151/13762501
+    return Text(
+      text,
+      style: TextStyle(
+        inherit: true,
+        fontSize: fontSize,
+        color: color,
+        shadows: [
+          Shadow(
+            // bottomLeft
+            offset: const Offset(-1.5, -1.5),
+            color: borderColor,
+          ),
+          Shadow(
+            // bottomRight
+            offset: const Offset(1.5, -1.5),
+            color: borderColor,
+          ),
+          Shadow(
+            // topRight
+            offset: const Offset(1.5, 1.5),
+            color: borderColor,
+          ),
+          Shadow(
+            // topLeft
+            offset: const Offset(-1.5, 1.5),
+            color: borderColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
