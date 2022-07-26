744c81ebc3558be113a44953d29e17457489c9ba
Rishabh Pathak <clusmsyknight@gmail.com>
2022-07-26 17:44:15 +0530

    mapped message related endpoints on flutter app

---
+++ lib/features/mesaging/services/messaging_services.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_chat_app/config/env.dart';
+import 'package:flutter_chat_app/constants/error_handling.dart';
+import 'package:flutter_chat_app/constants/utils.dart';
+import 'package:flutter_chat_app/models/message.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+class MessagingServices {
+  Future<List<Message>> getAllMessages({
+    required BuildContext context,
+    required String friendId,
+  }) async {
+    List<Message> messages = [];
+    try {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString(tokenKey);
+      http.Response res = await http.get(
+        Uri.parse('$uri/message/from/$friendId'),
+        headers: <String, String>{
+          'Content-type': "application/json; charset=UTF-8",
+          tokenKey: token!
+        },
+      );
+      var decodedRes = jsonDecode(res.body);
+      for (int i = 0; i < decodedRes.length; i++) {
+        var currentMessage = decodedRes[i];
+        Message message = Message.fromJson(jsonEncode(currentMessage));
+        messages.add(message);
+      }
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+    return messages;
+  }
+
+  Future<Message?> sendMessage({
+    required BuildContext context,
+    required String to,
+    required String text,
+  }) async {
+    Message? message;
+    try {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString(tokenKey);
+      http.Response res = await http.post(
+        Uri.parse('$uri/message'),
+        headers: <String, String>{
+          'Content-type': "application/json; charset=UTF-8",
+          tokenKey: token!
+        },
+        body: jsonEncode(
+          {
+            'to': to,
+            'text': text,
+          },
+        ),
+      );
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          message = Message.fromJson(jsonDecode(res.body));
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+    return message;
+  }
+}
