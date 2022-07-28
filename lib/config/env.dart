35b40d9a758b3d3a9688c73516fa871e5375ec32
Rishabh Pathak <clusmsyknight@gmail.com>
2022-07-28 21:33:57 +0530

    added env.dart

---
+++ lib/config/env.dart
@@ -0,0 +1,3 @@
+  String uri = 'http://<URI>:<PORT>'; // this will be your local ip and port where you will run `flutter-chat-api`
+  String tokenKey = "x-auth-token"; // string which should match your api's token key
+  String socketIdKey = "socketId"; // common string everywhere to store socketId
