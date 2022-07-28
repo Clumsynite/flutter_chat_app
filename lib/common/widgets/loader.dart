84a8d08239f3a893c57f9ea7d16ea77502608324
Rishabh Pathak <clusmsyknight@gmail.com>
2022-07-28 19:16:25 +0530

    added prop to change loader background color

--- lib/common/widgets/loader.dart
+++ lib/common/widgets/loader.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class Loader extends StatelessWidget {
-  const Loader({Key? key}) : super(key: key);
+  final Color color;
+  const Loader({
+    Key? key,
+    this.color = Colors.white,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
+      color: color,
       child: const Center(
         child: CircularProgressIndicator(),
       ),
