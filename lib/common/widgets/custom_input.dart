4f73eccd5bc7a55b2429ff508b8898593eb11e45
Rishabh Pathak <clusmsyknight@gmail.com>
2022-07-28 16:39:45 +0530

    added enabled prop in custom text input

--- lib/common/widgets/custom_input.dart
+++ lib/common/widgets/custom_input.dart
@@ -4,11 +4,13 @@ class CustomTextInput extends StatelessWidget {
   final String hintText;
   final TextEditingController controller;
   final int maxLines;
+  final bool enabled;
   const CustomTextInput({
     Key? key,
     required this.hintText,
     required this.controller,
     this.maxLines = 1,
+    this.enabled = true,
   }) : super(key: key);
 
   @override
@@ -16,6 +18,7 @@ class CustomTextInput extends StatelessWidget {
     return TextFormField(
       controller: controller,
       maxLines: maxLines,
+      enabled: enabled,
       decoration: InputDecoration(
         hintText: hintText,
         border: const OutlineInputBorder(
